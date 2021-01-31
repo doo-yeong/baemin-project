766ac9f0da64b3fe8e0405a3a5f9d3195c9d9eb6
dooyeong <dooyeong25@gmail.com>
2021-01-31 13:54:24 +0900

    test : git

--- server/baeminclone/src/main/java/com/dy/baeminclone/config/AppSecurityConfig.java
+++ server/baeminclone/src/main/java/com/dy/baeminclone/config/AppSecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        
 //        http.authorizeRequests()
 //                .antMatchers("/**").permitAll();
         http.cors().and().csrf().disable();
